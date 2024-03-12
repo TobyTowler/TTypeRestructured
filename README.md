436b1ef889433490e2d71fedda952a1470cdd195
TobyTowler <tobytowler@gmail.com>
2024-03-12 15:33:52 +0000

    new line detected and something else i think

--- README.md
+++ README.md
@@ -1,25 +1,27 @@
 # TType - Rebuilt to be object-oriented
 # About 
-- my first large unguided project (technicall TType was but this is a contiuation)
+- my first large unguided project (technically TType was but this is a continuation)
 - very open to any advice
-- can use any .txt file as long its text directory or path is specified on input
+- can use any .txt file where:
+    - file is in text directory
+    - all text is on 1 line, new line characters will cause the program to stop even if more text is displayed
 
 # Releases
 ## v1.2
-- Score now shows raw WPN and accurate WPM
+- Score now shows raw WPM and accurate WPM
 - Text files are now stored in "text" directory
 - Added start button to title screen
 
 ## V1.1
 - Added raw words per minute score
   - (total words/time)
-  - regardless of accuracry
+  - regardless of accuracy
 - Added line wrapping to avoid being cut in half for a new line(most of the time)
 - changed title to require a specific key to be pressed to start the game
 - Reformatted the score display
 
 ## V1.0
-- first publishment
+- first release
 - rebuilt to be object/class based and have better modularity
 
 # Requirements
@@ -28,8 +30,6 @@
 # Screenshots
 ### Title screen
 ![image](https://github.com/TobyTowler/TTypeRestructured/assets/135618916/9f0d6586-4fb2-4342-8f9e-32490daa8cfb)
-
-
 
 ### Playing
 ![image](https://github.com/TobyTowler/TTypeRestructured/assets/135618916/d49bc16b-0cab-439b-aba9-2ccd7c9c35ca)
--- src/TType.cpp
+++ src/TType.cpp
@@ -2,16 +2,18 @@
 #include "chrono"
 #include "ncurses.h"
 #include <chrono>
+#include <cstring>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
 // Custom text
 void TType::setWords(string fileName) {
-  stringstream str;
-  std::ifstream inputFile("text/" + fileName);
-  str << inputFile.rdbuf();
-  inputFile.close();
-  words = str.str();
+    stringstream str;
+    std::ifstream inputFile("text/" + fileName);
+    str << inputFile.rdbuf();
+    inputFile.close();
+    words = str.str();
 }
 
 string TType::getWords() { return words; }
@@ -26,60 +28,60 @@ void TType::clearInput() { input.clear(); }
  *************************************/
 void TType::checkChar() {
 
-  int length = 0, height, width;
+    int length = 0, height, width;
 
-  getmaxyx(stdscr, height, width);
+    getmaxyx(stdscr, height, width);
 
-  clear();
+    clear();
 
-  // Colour pairs definition
-  short GREEN = 1, RED = 2, WHITE = 3, MAGENTA = 4;
-  init_pair(1, COLOR_GREEN, COLOR_BLACK);
-  init_pair(2, COLOR_RED, COLOR_BLACK);
-  init_pair(3, COLOR_WHITE, COLOR_BLACK);
-  init_pair(4, COLOR_WHITE, COLOR_MAGENTA);
+    // Colour pairs definition
+    short GREEN = 1, RED = 2, WHITE = 3, MAGENTA = 4;
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    init_pair(2, COLOR_RED, COLOR_BLACK);
+    init_pair(3, COLOR_WHITE, COLOR_BLACK);
+    init_pair(4, COLOR_WHITE, COLOR_MAGENTA);
 
-  // Chars user has written - RED-GREEN/BLACK
-  for (int i = 0; i < input.size(); i++) {
-    length++;
-    // width-10 is a rough value thast seems to look decent since most words
-    // arent over 10 characters long could make it dynamic based on distance to
-    // next space in words
-    if (length > width - 10 && words.at(i - 1) == ' ') {
-      printw("\n");
-      length = 0;
+    // Chars user has written - RED-GREEN/BLACK
+    for (int i = 0; i < input.size(); i++) {
+        length++;
+        // width-10 is a rough value thast seems to look decent since most words
+        // arent over 10 characters long could make it dynamic based on distance
+        // to next space in words
+        if (length > width - 10 && words.at(i - 1) == ' ') {
+            printw("\n");
+            length = 0;
+        }
+
+        if (words.at(i) == input.at(i)) {
+            attron(COLOR_PAIR(GREEN));
+            printw("%c", words.at(i));
+            attroff(COLOR_PAIR(GREEN));
+        } else {
+            attron(COLOR_PAIR(RED));
+            printw("%c", words.at(i));
+            attroff(COLOR_PAIR(RED));
+        }
     }
 
-    if (words.at(i) == input.at(i)) {
-      attron(COLOR_PAIR(GREEN));
-      printw("%c", words.at(i));
-      attroff(COLOR_PAIR(GREEN));
-    } else {
-      attron(COLOR_PAIR(RED));
-      printw("%c", words.at(i));
-      attroff(COLOR_PAIR(RED));
-    }
-  }
+    // Current char - WHITE/MAGENTA
+    attron(COLOR_PAIR(MAGENTA));
+    printw("%c", words[input.size()]);
+    attroff(COLOR_PAIR(MAGENTA));
 
-  // Current char - WHITE/MAGENTA
-  attron(COLOR_PAIR(MAGENTA));
-  printw("%c", words[input.size()]);
-  attroff(COLOR_PAIR(MAGENTA));
+    // Chars not yet reached - WHITE/BLACK
+    for (int o = input.size() + 1; o < words.length(); o++) {
+        length++;
+        if (length > width - 10 && words.at(o - 1) == ' ') {
+            printw("\n");
+            length = 0;
+        }
 
-  // Chars not yet reached - WHITE/BLACK
-  for (int o = input.size() + 1; o < words.length(); o++) {
-    length++;
-    if (length > width - 10 && words.at(o - 1) == ' ') {
-      printw("\n");
-      length = 0;
+        attron(COLOR_PAIR(WHITE));
+        printw("%c", words[o]);
+        attroff(COLOR_PAIR(WHITE));
     }
 
-    attron(COLOR_PAIR(WHITE));
-    printw("%c", words[o]);
-    attroff(COLOR_PAIR(WHITE));
-  }
-
-  refresh();
+    refresh();
 }
 
 vector<int> TType::getInput() { return input; }
@@ -87,11 +89,11 @@ vector<int> TType::getInput() { return input; }
 void TType::addToInput(char c) { input.push_back(c); }
 
 TType::TType() {
-  this->setWords("default.txt");
-  this->input = {};
-  this->ch = '\0';
-  this->runTime = 0;
-  this->numberOfCorrectWords = 0;
+    this->setWords("default.txt");
+    this->input = {};
+    this->ch = '\0';
+    this->runTime = 0;
+    this->numberOfCorrectWords = 0;
 }
 
 int TType::getCh() const { return ch; }
@@ -100,45 +102,54 @@ void TType::setCh(int ch) { this->ch = ch; }
 
 void TType::run() {
 
-  int length = 0; // holds the line length
+    bool started = false;
+    int length = 0; // holds the line length
 
-  auto start = std::chrono::steady_clock::now();
+    chrono::steady_clock::time_point start;
 
-  for (int i = 0; i < size(words); i++) {
+    for (int i = 0; i < size(words); i++) {
+        if (words[i] == '\n') {
+            break;
+        }
+        checkChar();
+        ch = getch();
 
-    checkChar();
-    ch = getch();
-    input.push_back(ch);
-    // addToInput(ch);
+        if (!started) {
+            start = std::chrono::steady_clock::now();
+            started = true;
+        }
 
-    if (ch == KEY_BACKSPACE) {
-      if (i > 0)
-        i -= 2;
-      else
-        i--;
-      input.pop_back();
-      input.resize(i + 1);
+        input.push_back(ch);
+        // addToInput(ch);
+
+        if (ch == KEY_BACKSPACE) {
+            if (i > 0)
+                i -= 2;
+            else
+                i--;
+            input.pop_back();
+            input.resize(i + 1);
+        }
     }
-  }
 
-  auto end = std::chrono::steady_clock::now();
-  std::chrono::duration<double> time = (end - start);
-  runTime = time.count();
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time = (end - start);
+    runTime = time.count();
 
-  checkCharAndRealWPM();
-  printScore();
+    checkCharAndRealWPM();
+    printScore();
 }
 
 double TType::getRunTime() { return runTime; }
 
 int TType::getNumberOfSpaces() {
-  int num = 0;
-  for (char c : words) {
-    if (c == ' ') {
-      num++;
+    int num = 0;
+    for (char c : words) {
+        if (c == ' ') {
+            num++;
+        }
     }
-  }
-  return num;
+    return num;
 }
 
 void TType::setNumberOfCorrectWords(int num) { numberOfCorrectWords = num; }
@@ -148,79 +159,79 @@ int TType::getNumberOfCorrectWords() { return numberOfCorrectWords; }
 int TType::getRawWPM() { return int((getNumberOfSpaces() + 1) * 60 / runTime); }
 
 int TType::getAccuracy() {
-  int right = 0;
+    int right = 0;
 
-  for (int i = 0; i < input.size(); i++) {
-    if (words.at(i) == input.at(i)) {
-      right++;
+    for (int i = 0; i < input.size(); i++) {
+        if (words.at(i) == input.at(i)) {
+            right++;
+        }
     }
-  }
 
-  return (double(right) / double(words.size()) * 100);
+    return (double(right) / double(words.size()) * 100);
 }
 
 void TType::checkCharAndRealWPM() {
 
-  int length = 0, height, width;
+    int length = 0, height, width;
 
-  getmaxyx(stdscr, height, width);
+    getmaxyx(stdscr, height, width);
 
-  clear();
+    clear();
 
-  // Colour pairs definition
-  short GREEN = 11, RED = 10;
-  init_pair(11, COLOR_GREEN, COLOR_BLACK);
-  init_pair(10, COLOR_RED, COLOR_BLACK);
+    // Colour pairs definition
+    short GREEN = 11, RED = 10;
+    init_pair(11, COLOR_GREEN, COLOR_BLACK);
+    init_pair(10, COLOR_RED, COLOR_BLACK);
 
-  // Chars user has written - RED-GREEN/BLACK
-  for (int i = 0; i < input.size(); i++) {
-    length++;
-    // width-10 is a rough value thast seems to look decent since most words
-    // arent over 10 characters long could make it dynamic based on distance to
-    // next space in words
-    if (length > width - 10 && words.at(i - 1) == ' ') {
-      printw("\n");
-      length = 0;
+    // Chars user has written - RED-GREEN/BLACK
+    for (int i = 0; i < input.size(); i++) {
+        length++;
+        // width-10 is a rough value thast seems to look decent since most words
+        // arent over 10 characters long could make it dynamic based on distance
+        // to next space in words
+        if (length > width - 10 && words.at(i - 1) == ' ') {
+            printw("\n");
+            length = 0;
+        }
+
+        if (words.at(i) == input.at(i)) {
+            attron(COLOR_PAIR(GREEN));
+            printw("%c", words.at(i));
+            attroff(COLOR_PAIR(GREEN));
+        } else {
+            attron(COLOR_PAIR(RED));
+            printw("%c", words.at(i));
+            attroff(COLOR_PAIR(RED));
+        }
     }
 
-    if (words.at(i) == input.at(i)) {
-      attron(COLOR_PAIR(GREEN));
-      printw("%c", words.at(i));
-      attroff(COLOR_PAIR(GREEN));
-    } else {
-      attron(COLOR_PAIR(RED));
-      printw("%c", words.at(i));
-      attroff(COLOR_PAIR(RED));
+    bool CORRECT = true;
+    for (int i = 0; i < input.size(); i++) {
+        if (words.at(i) != input.at(i)) {
+            CORRECT = false;
+        }
+        if (words.at(i) == ' ' || i == words.length() - 1) {
+            if (CORRECT) {
+                numberOfCorrectWords++;
+            }
+            CORRECT = true;
+        }
     }
-  }
-
-  bool CORRECT = true;
-  for (int i = 0; i < input.size(); i++) {
-    if (words.at(i) != input.at(i)) {
-      CORRECT = false;
-    }
-    if (words.at(i) == ' ' || i == words.length() - 1) {
-      if (CORRECT) {
-        numberOfCorrectWords++;
-      }
-      CORRECT = true;
-    }
-  }
-  refresh();
+    refresh();
 }
 
 void TType::resetGame() {
-  input.clear();
-  numberOfCorrectWords = 0;
+    input.clear();
+    numberOfCorrectWords = 0;
 }
 
 void TType::printScore() {
-  printw("\n\n");
-  printw("--GAME OVER--\nYou Scored:\n\n");
-  printw("  Accuracy: %d%%", getAccuracy());
-  printw("        WPM: %d", int((numberOfCorrectWords * 60) / runTime));
-  printw("        Raw WPM: %d", int((getNumberOfSpaces() + 1) * 60 / runTime));
-  printw("\n\n\n\n");
+    printw("\n\n");
+    printw("--GAME OVER--\nYou Scored:\n\n");
+    printw("  Accuracy: %d%%", getAccuracy());
+    printw("        WPM: %d", int((numberOfCorrectWords * 60) / runTime));
+    printw("        Raw WPM: %d", int((getNumberOfSpaces() + 1) * 60 / runTime));
+    printw("\n\n\n\n");
 
-  refresh();
+    refresh();
 }
